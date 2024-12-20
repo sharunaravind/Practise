7331423b0d626daeba3e6e33fe89420a96f9a676
sharun <sharun.offl@gmail.com>
2024-12-20 07:26:23 +0530

    idk

---
+++ DSA/tests/scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void main()
+{
+    int arr[]={12,78,32,5,4,54,4,24};
+    int n=0;
+    int count = scanf("%d",&n);
+    printf("%d %d",count,n);
+    printf(" %d",getc(stdin));
+
+
+}
